caa0e8c40175679b05e5d33fd2200b8651f06b83
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-07 19:23:21 -0400

    adding basic functions

---
+++ script.js
@@ -0,0 +1,18 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+const promptUser = () =>{
+    return inquirer.prompt([
+        {
+
+        }
+    ])
+}
+
+let generateHTML = ({name, location, github, linkedin}) => {
+
+}
