aae3759dee5796ffb083bf26289a23a826846ab3
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-07 19:31:34 -0400

    adding async call to generate html from user input

--- script.js
+++ script.js
@@ -8,11 +8,55 @@ const writeFileAsync = util.promisify(fs.writeFile);
 const promptUser = () =>{
     return inquirer.prompt([
         {
+            type: 'input',
+            name: 'name',
+            message: 'what is your name?'
 
-        }
+        },
+        {
+            type: 'input',
+            name: 'location',
+            message: 'where are you from'
+        },
+        {
+            type: "input",
+            name: "hobby",
+            message: "What is your favorite hobby?"
+          },
+          {
+            type: "input",
+            name: "food",
+            message: "What is your favorite food?"
+          },
+          {
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub Username"
+          },
+          {
+            type: "input",
+            name: "linkedin",
+            message: "Enter your LinkedIn URL."
+          }
+
     ])
 }
 
 let generateHTML = ({name, location, github, linkedin}) => {
+    
 
 }
+
+const init = async() => {
+    console.log('hi')
+    try {
+        const answers = await promptUser()
+        console.log(answers)
+        const html =  generateHTML(answers)
+        await writeFileAsync('index.html', html)
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+init()
